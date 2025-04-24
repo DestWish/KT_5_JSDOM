b79367ff5941bab72aa655b37974da963fc4b05f
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2025-04-24 11:41:33 +0300

    тестовый коммит 2

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const form = document.forms.name;
-const div = form.nextElementSibling;
-form.addEventListener("submit", function(e) {
-	e.preventDefault();
-	let inp = form.firstElementChild;
-	if (inp.value) {
-		div.innerText = "Привет, " + inp.value + "!";
-	}
-});
+fkfj;sldfjsl
